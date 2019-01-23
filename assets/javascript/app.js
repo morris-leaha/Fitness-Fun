e225525c4fcada0e3c7c24cc01a38be011cecf17
Tom Johnson <tomajohnson21@gmail.com>
2019-01-22 20:29:01 -0600

    Added on-click function for sign-up-btn

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,3 +8,28 @@ var config = {
   };
 
 firebase.initializeApp(config);
+
+$("#sign-up-btn").on("click", function(){
+
+    var email;
+    var firstName;
+    var lastName;
+    var username;
+    var password;
+    var confirmPass;
+
+    if(email && username && firstName && lastName && password && confirmPass && confirmPass === password){
+        
+        firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error){
+            var errorCode = error.code;
+            var errorMessage = error.message;
+
+            console.log(errorCode);
+            console.log(errorMaster);
+        });
+    } else {
+
+        console.log("Create user failed");
+    }
+    
+});
