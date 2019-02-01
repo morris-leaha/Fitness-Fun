b37d4d4d922cdf97c6d2fb789d24d25726c98db5
LAMorris <morris.leaha@gmail.com>
2019-01-31 22:09:57 -0600

    working on exercise.js

--- assets/javascript/exercise.js
+++ assets/javascript/exercise.js
@@ -12,24 +12,31 @@ var config = {
 firebase.initializeApp(config);
 
 var dataRef = firebase.database();
-
+var uid = sessionStorage.getItem("uid");
+console.log(uid);
+var exerciseRef = "/" + uid + "/exercise";
+var totalBurnedCals = 0;
 var calsPerExercise = {};
+var currentDate = moment().format("YYYY-MM-DD");
+
 
 //Form Validation
 var validateExerciseForm = function (exercise) {
 
-    if (!exercise.classification ||
-        !exercise.exercisename ||
-        !exercise.exerciseduration ||
-        //!exercise.exercisecalsburned ||
-        !exercise.exercisecurrentTime
-    ) {
+    if (
+        !exercise.exerciseName ||
+        !exercise.exerciseDuration ||
+        !exercise.exerciseCalsBurned ||
+        !exercise.entryTime ||
+        !exercise.entryDate ||
+        isNaN(exercise.exerciseDuration) ||
+        isNaN(exercise.exerciseCalsBurned)) {
         console.log("Form Failed");
     } else {
-        dataRef.ref().push(exercise)
+        dataRef.ref(exerciseRef).push(exercise)
         $("#exercise-name-input").val("");
         $("#exercise-duration-input").val("");
-       // $("#exercise-cals-burned-input").val("");
+        $("#exercise-cals-burned-input").val("");
     }
 }
 // adding exercise to table 
@@ -37,56 +44,72 @@ $("#add-exercise-btn").on("click", function (event) {
     event.preventDefault();
 
     // get input data
-    var classification = "exercise";
-    var exercisename = $("#exercise-name-input").val().trim();
-    var exerciseduration = $("#exercise-duration-input").val().trim();
-    //var exercisecalsburned = $("#exercise-cals-burned-input").val().trim();
-    var exercisecurrentTime = moment().format("YYYY-MM-DD HH:mm Z");
+    var exerciseName = $("#exercise-name-input").val().trim();
+    var exerciseDuration = $("#exercise-duration-input").val().trim();
+    var exerciseCalsBurned = $("#exercise-cals-burned-input").val().trim();
+    var entryTime = moment().format("YYYY-MM-DD HH:mm");
+    var entryDate = moment().format("YYYY-MM-DD");
 
-    console.log(exercisename);
-    console.log(exerciseduration);
-    //console.log(exercisecalsburned);
-    console.log(exercisecurrentTime);
-    //console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+    console.log(exerciseName);
+    console.log(exerciseDuration);
+    console.log(exerciseCalsBurned);
+    console.log(entryTime);
+    console.log(entryDate);
 
     // Creates local "temporary" object for holding data
     var exerciseInfo = {
-        classification: classification,
-        exercisename: exercisename,
-        exerciseduration: exerciseduration,
-        //exercisecalsburned: exercisecalsburned,
-        exercisecurrentTime: exercisecurrentTime
+        exerciseName: exerciseName,
+        exerciseDuration: exerciseDuration,
+        exerciseCalsBurned: exerciseCalsBurned,
+        entryDate: entryDate,
+        entryTime: entryTime
     };
 
     validateExerciseForm(exerciseInfo);
 });
 
+var appendTable = function (exerciseObject) {
+    var exerciseRow = $("<tr>").append(
+
+        $("<td>").text(exerciseObject.exerciseName),
+        $("<td>").text(exerciseObject.exerciseDuration),
+    )
+
+    $("#exercise-table > tbody").append(exerciseRow);
+    totalBurnedCals += parseInt(exerciseObject.exerciseCalsBurned);
+    $("#total-cal").text(totalCal);
+}
+
 // Create Firebase event for adding to a row in the html when a user adds an entry
-dataRef.ref().on("child_added", function (childSnapshot) {
-    //console.log(childSnapshot.val());
-    var userFirstName = childSnapshot.val().firstName;
-    $("#nav-username").text(userFirstName);
-    var exercisedurationdb = childSnapshot.val().exerciseduration;
-    //var exercisecalsburneddb = childSnapshot.val().exercisecalsburned;
-    var exercisenamedb = childSnapshot.val().exercisename;
+dataRef.ref(exerciseRef).on("child_added", function (childSnapshot) {
 
+    // if(currentDate) {
+    //     var exerciseObject = childSnapshot.val();
+    //     var initDate = moment(exerciseObject.entryTime, "YYYY-MM-DD");
+    //     var currentTime = moment();
+    //     var convCurrTime = moment(currentTime, "YYYY-MM-DD");
+    //     var dateDiff = convCurrTime.diff(initDate, "days");
+    //     if(dateDiff < 8){
+    //         appendTable(exerciseObject);
+    //     }
+    // });
 
-    // var exerciseData = snapshot.val().exercise;
-    // var durationData = snapshot.val().duration;
-    // var burnCalorieData = snapshot.val().burnCalorie;
+    var exerciseDurationDB = childSnapshot.val().exerciseDuration;
+    var exerciseCalsBurnedDB = childSnapshot.val().exerciseCalsBurned;
+    var exerciseNameDB = childSnapshot.val().exerciseName;
 
 
     // Create the new row
-    if (childSnapshot.val().classification === "exercise") {
-        var exerciseRow = $("<tr>").append(
 
-            $("<td>").text(exercisenamedb),
-            $("<td>").text(exercisedurationdb),
-            //$("<td>").text(exercisecalsburneddb),
-        );
+    var exerciseRow = $("<tr>").append(
 
-        // Append the new row to the table
-        $("#exercise-table > tbody").append(exerciseRow);
-    }
+        $("<td>").text(exerciseNameDB),
+        $("<td>").text(exerciseDurationDB),
+        $("<td>").text(exerciseCalsBurnedDB),
+    );
+
+    // Append the new row to the table
+    $("#exercise-table > tbody").append(exerciseRow);
+
 
 });
--- assets/javascript/nutrition.js
+++ assets/javascript/nutrition.js
@@ -1,23 +1,5 @@
 //Nutritionix API: 68bd18dff81e4268ce3dd68b02a4f509
 
-(function() {
-  'use strict';
-  window.addEventListener('load', function() {
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.getElementsByClassName('needs-validation');
-    // Loop over them and prevent submission
-    var validation = Array.prototype.filter.call(forms, function(form) {
-      form.addEventListener('submit', function(event) {
-        if (form.checkValidity() === false) {
-          event.preventDefault();
-          event.stopPropagation();
-        }
-        form.classList.add('was-validated');
-      }, false);
-    });
-  }, false);
-})();
-
 // Initialize Firebase
   var config = {
     apiKey: "AIzaSyDgFgcmDm5gwBlUoc9cv6174w5gHjiPkU0",
