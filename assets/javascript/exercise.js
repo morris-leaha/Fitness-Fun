fee7c430ef1009aad6bb26bf52b320dbd90d012d
Ferat Imrak <feratimrak1@gmail.com>
2019-02-02 03:17:36 -0600

    worked on youtube api

--- assets/javascript/exercise.js
+++ assets/javascript/exercise.js
@@ -14,9 +14,9 @@ firebase.initializeApp(config);
 var dataRef = firebase.database();
 var uid = sessionStorage.getItem("uid");
 var exerciseRef = "/" + uid + "/exercise";
-console.log(exerciseRef);
+//console.log(exerciseRef);
 var profileRef = "/" + uid + "/profile";
-console.log(profileRef);
+//console.log(profileRef);
 var userWeight;
 
 var calsPerExercise = {};
@@ -26,7 +26,7 @@ dataRef.ref(profileRef).on("value", function(snapshot){
     snapshot.forEach(function(childSnapshot){
         console.log(childSnapshot.val());
         userWeight = childSnapshot.val().userweight;
-        console.log(userWeight);    
+        //console.log(userWeight);    
     });
 });
 
@@ -38,7 +38,7 @@ var validateExerciseForm = function (exercise) {
         !exercise.calsBurn ||
         !exercise.exercisecurrentTime
     ) {
-        console.log("Form Failed");
+        //console.log("Form Failed");
     } else {
         dataRef.ref(exerciseRef).push(exercise)
         $("#exercise-name-input").val("");
@@ -49,30 +49,27 @@ var validateExerciseForm = function (exercise) {
 // adding exercise to table 
 $("#add-exercise-btn").on("click", function (event) {
     event.preventDefault();
-    //console.log("hello");
     // get input data
-    // var classification = "exercise";
     var exercisename = $(".exercise-name-input option:selected").val();
     var exerciseMet = $(".exercise-name-input option:selected").attr("data-met");
     console.log(exerciseMet)
     var exerciseduration = $("#exercise-duration-input").val().trim();
-    //var exercisecalsburned = $("#exercise-cals-burned-input").val().trim();
     var exercisecurrentTime = moment().format("YYYY-MM-DD HH:mm Z");
 
+
+    //Met value formula.
     var calsBurn = Math.round(exerciseMet * (exerciseduration / 60) * (userWeight / 2.2));
     console.log(calsBurn);
 
-    console.log(exercisename);
-    console.log(exerciseduration);
-    //console.log(exercisecalsburned);
-    console.log(exercisecurrentTime);
+    // console.log(exercisename);
+    // console.log(exerciseduration);
+    // console.log(exercisecurrentTime);
     //console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
 
     // Creates local "temporary" object for holding data
     var exerciseInfo = {
         exercisename: exercisename,
         exerciseduration: exerciseduration,
-        //exercisecalsburned: exercisecalsburned,
         calsBurn: calsBurn,
         exercisecurrentTime: exercisecurrentTime
     };
@@ -82,22 +79,15 @@ $("#add-exercise-btn").on("click", function (event) {
 
 // Create Firebase event for adding to a row in the html when a user adds an entry
 dataRef.ref(exerciseRef).on("child_added", function (childSnapshot) {
-    console.log(childSnapshot.val());
+    //console.log(childSnapshot.val());
     var userFirstName = childSnapshot.val().firstName;
     $("#nav-username").text(userFirstName);
     var exercisedurationdb = childSnapshot.val().exerciseduration;
-    //var exercisecalsburneddb = childSnapshot.val().exercisecalsburned;
     var calsBurndb = childSnapshot.val().calsBurn;
     var exercisenamedb = childSnapshot.val().exercisename;
 
 
-    // var exerciseData = childSnapshot.val().exercise;
-    // var durationData = childSnapshot.val().duration;
-    // var burnCalorieData = childSnapshot.val().exercisecalsburned;
     
-    
-
-
     // Create the new row
 
     var exerciseRow = $("<tr>").append(
@@ -107,10 +97,79 @@ dataRef.ref(exerciseRef).on("child_added", function (childSnapshot) {
         $("<td>").text(calsBurndb),
     );
 
-    console.log(exerciseRow);
+    //console.log(exerciseRow);
 
     // Append the new row to the table
     $("#exercise-table > tbody").append(exerciseRow);
 
 
 });
+
+var pageToken = "";
+
+
+$(document).ready(function() {
+
+    $(".popup").hide();
+    $(".overlayBg").hide();
+     
+    $("#search-submit").click(function(){
+    
+        searchYoutube();
+        
+    });
+
+    $(".tokenClass").click(function(){
+
+    })
+
+});
+
+$(".overlayBg").click(function(){
+    $(".popup").hide();
+    $(".overlayBg").hide();
+})
+
+$("#output").on("click",".thumbnail", function(){
+    $(".popup").show();
+    $(".overlayBg").show();
+    $(window).scrollTop(0);
+    
+    $(".popup iframe").attr("src","https://www.youtube.com/embed/"+$(this).attr('videoID'));
+})
+    
+
+
+function searchYoutube(){
+
+    $.ajax({
+        url:"https://www.googleapis.com/youtube/v3/search",
+        dataType:'json',
+        type:'GET',
+        data:{
+            key:'AIzaSyAZJ-ZKG8sb46Z8nFK39RBtpZGqmjEWdOA',
+            q: $("#search-input").val(),
+            part:'snippet',
+            maxResults:5,
+            pageToken: pageToken.current
+        }
+    }).done(function(data){
+        pageToken.nextPage = data.nextPageToken;
+        pageToken.prevPage = data.prevPageToken;
+        var html = "";
+
+        $.each(data["items"],function(index,value){
+
+            html += '<div><div class="title">' + value.snippet.title + '</div>';
+            html += '<div><div class="url"><a href="https://www.youtube.com/watch?v=' + value.id.videoId +'" target="_blank">' + value.id.videoId + '</a></div>';
+            html += '<div><img class="thumbnail" src="'+value.snippet.thumbnails.medium.url+'" videoID="'+value.id.videoId+'"></div>';
+            html += '</div>';
+
+            
+        })
+        $("#output").html(html);
+    });
+};
+
+
+//https://www.googleapis.com/youtube/v3/search?q=test&part=snippet&key=AIzaSyAZJ-ZKG8sb46Z8nFK39RBtpZGqmjEWdOA
